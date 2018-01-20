5ea81ab9864eb05e7509666b837e3863ca91487c
David Corkett <dcorkett@gmail.com>
2018-01-20 17:45:14 +0000

    model-component $emit to close modal

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
 /* global Vue*/
 
-Vue.component('', {
-//slot as template for li
-  template: ``,
-
-  data() {
-    return {
-
-    };
-  }
-});
-
-Vue.component('', {
-//slot as template for li
-  template: ''
+Vue.component('modal', {
+  template: `
+  <div class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <div class="box">
+        <slot></slot>
+      </div>
+    </div>
+    <button class="modal-close is-large" aria-label="close" @click="$emit('close-modal')"></button>
+  </div>
+  `
 });
 
 new Vue({
-  el: '#root'
+  el: '#root',
+
+  data: {
+    showModal: false
+  }
 });
