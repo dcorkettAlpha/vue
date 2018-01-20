792da8e8ecf5c964402c8a3eb05a675e78673735
David Corkett <dcorkett@gmail.com>
2018-01-20 17:26:27 +0000

    blank component initial

--- js/main.js
+++ js/main.js
@@ -1,28 +1,19 @@
 /* global Vue*/
 
-Vue.component('task-list', {
+Vue.component('', {
 //slot as template for li
-  template: `<div>
-              <task v-for="(task, key) in tasks" :key="key"> {{ task.description }}</task>
-            </div>`,
+  template: ``,
 
   data() {
     return {
-      tasks: [
-        { description: 'Go to the store', completed: true},
-        { description: 'Finish Tutorial', completed: false},
-        { description: 'Eat', completed: false},
-        { description: 'Sleep', completed: false},
-        { description: 'Be Merry', completed: false},
-        { description: 'Drink lots of water', completed: true}
-      ]
+
     };
   }
 });
 
-Vue.component('task', {
+Vue.component('', {
 //slot as template for li
-  template: '<li><slot></slot></li>'
+  template: ''
 });
 
 new Vue({
